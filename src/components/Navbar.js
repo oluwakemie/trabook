8831f67bf36d9bca7ca495b029fa291a0dc1784f
Oluwakemi <oluwakemie05@gmail.com>
2023-09-14 13:50:22 +0100

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const Navbar = () => {
+  const [menu, setMenu] = useState(false);
+
   return (
     <div className="flex justify-between items-center">
-      <div className="font-bold flex gap-2 ">
+      <div className="font-bold flex gap-2  ">
         <h1>Trabook</h1>
         <img src="/Images/logo.svg" alt="" />
       </div>
-      <nav className="text-xs md:flex hidden  ">
-        <ul className="flex gap-4">
+      <nav className=" md:flex flex-col text-xs  md:flex-row hidden">
+        <ul className="md:flex gap-4 lg:flex  ">
           <Link to="/">
             <li>Home</li>
           </Link>
 
-
           <Link to="/about">
             <li>About </li>
           </Link>
-
 
           <Link to="/picture">
             <li>Destination</li>
@@ -27,7 +28,7 @@ const Navbar = () => {
           <li>Blog</li>
         </ul>
       </nav>
-      <div className="text-xs flex gap-7 font-medium">
+      <div className="text-xs hidden md:flex gap-7 font-medium">
         <button type="button" className="text-[#FA7436]">
           Login
         </button>
@@ -39,6 +40,7 @@ const Navbar = () => {
         </button>
       </div>
       <svg
+        onClick={() => setMenu(!menu)}
         className="block md:hidden"
         width="24"
         height="24"
@@ -65,6 +67,38 @@ const Navbar = () => {
           stroke-linecap="round"
         />
       </svg>
+      {menu ? (
+        <motion.div
+        initial={{ x: -500 }}
+        animate={{ x: 0 }}
+          transition={{
+            duration: 2.0,
+            type: "spring",
+            stiffness: 260,
+            damping: 20,
+          }}
+          className="absolute top-[56px] right-0 min-w-[190px] md:hidden  bg-[#FA7436] "   
+
+        >
+          <ul className="md:flex gap-7 lg:flex px-8 py-5 items-center justify-center text-white ">
+            <Link to="/">
+              <li>Home</li>
+            </Link>
+
+            <Link to="/about">
+              <li>About </li>
+            </Link>
+
+            <Link to="/picture">
+              <li>Destination</li>
+            </Link>
+            <li>Tour</li>
+            <li>Blog</li>
+          </ul>
+        </motion.div>
+      ) : (
+        ""
+      )}
     </div>
   );
 };
